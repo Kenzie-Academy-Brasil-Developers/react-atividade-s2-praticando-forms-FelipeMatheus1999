f93b3d2217c308d519edab17cce47130a64d09e5
Felipe Matheus <felipe_cavalcante7939@hotmail.com>
2021-09-16 12:10:22 -0300

    Adding tweaks

--- forms/src/App.js
+++ forms/src/App.js
@@ -2,15 +2,16 @@ import "./App.css";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { string } from "yup/lib/locale";
 
 function App() {
   const formSchema = yup.object().shape({
     username: yup.string().required("Username required"),
     fullName: yup.string().required("fullName required"),
     email: yup.string().required("Email required").email(),
+    emailConfirm: yup.string().oneOf([yup.ref("email")], "Emails not equals"),
     password: yup.string().required("password required"),
-    passwordconfirm: yup.string().required("confirm the password"),
+    passwordConfirm: yup.string()
+    .oneOf([yup.ref("password")], "Passwords not equals"),
   });
 
   const {
@@ -22,13 +23,7 @@ function App() {
   });
 
   const onSubmitFunction = (data) => {
-    // return (
-    //   <div className="card">
-    //     <span className="card__span" >{data.username}</span>
-    //     <span className="card__span" >{data.fullName}</span>
-    //     <span className="card__span" >{data.email}</span>
-    //   </div>
-    // );
+    //Fazer um card com os dados do form!
     console.log(data);
   };
 
@@ -42,41 +37,50 @@ function App() {
             {...register("username")}
             className="form__input"
           ></input>
-          {errors.username?.message}
+          {errors.username?.message && (
+            <p className="form__error">Username required</p>
+          )}
           <input
             type="name"
             placeholder="Full name *"
             {...register("fullName")}
             className="form__input"
           ></input>
-          {errors.fullName?.message}
+          {errors.fullName?.message && (
+            <p className="form__error">Full name required</p>
+          )}
           <input
             type="email"
             placeholder="Email *"
             {...register("email")}
             className="form__input"
           ></input>
-          {errors.email?.message}
+          {errors.email?.message && (
+            <p className="form__error">Email required</p>
+          )}
           <input
             type="email"
-            placeholder="Email confirmation"
+            placeholder="Email confirmation *"
+            {...register("emailConfirm")}
             className="form__input"
           ></input>
+          {errors.emailConfirm?.message && (
+            <p className="form__error">Email not equal</p>
+          )}
           <input
             type="password"
-            placeholder="Password"
+            placeholder="Password *"
             {...register("password")}
             className="form__password"
           ></input>
-          {errors.password?.message}
           <input
             type="password"
-            placeholder="Password confirm"
-            {...register("passwordconfirm")}
+            placeholder="Password confirm *"
+            {...register("passwordConfirm")}
             className="form__password"
           ></input>
-          {errors.passwordconfirm?.message}
           <input type="checkbox" className="form__button--checkbox"></input>
+          <p className="checkbox__message">I accept the terms and conditions</p>
           <button type="submit" className="form__button">
             submit
           </button>
